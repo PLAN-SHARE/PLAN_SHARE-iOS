3b5d4a880c4801c261b41a20630b01e393d3ffaa
박도윤 <doyun0887@gmail.com>
2022-02-23 14:53:24 +0900

    :wrench: config: Extensions 파일 추가        body: TextField left/right padding 기능 추가

---
+++ PlanShare_iOS/Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  Extensions.swift
+//  PlanShare_iOS
+//
+//  Created by doyun on 2022/02/23.
+//
+
+import Foundation
+import UIKit
+
+extension UITextField {
+    func setLeftPaddingPoints(_ amount:CGFloat){
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    func setRightPaddingPoints(_ amount:CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
+}
