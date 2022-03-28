4e67150c7593bc3c74696fa7f3ab7bafc7ad85db
doyun0887@gmail.com <doyun>
2022-03-29 01:52:20 +0900

    :sparkles: feat : 유저정보 API 기능 구현
           body: 서버 개발됨에 따라 유저정보 API 구현

--- PlanShare_iOS/Services/UserService.swift
+++ PlanShare_iOS/Services/UserService.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import RxSwift
+import Alamofire
 
 protocol UserSerivceProtocol {
     func fetchUser() -> Observable<[Member]>
@@ -18,14 +19,67 @@ class UserService : UserSerivceProtocol{
         
         return Observable.create { observer in
 
-            observer.onNext([
-                Member(id: 0, email: "doyun@gmail.com", kakaoId: 0, nickName: "dino"),
-                Member(id: 1, email: "doyun@gmail.com", kakaoId: 2, nickName: "asdf"),
-                Member(id: 2, email: "doyun@gmail.com", kakaoId: 3, nickName: "gerg")
-            ])
-
+            var memberInfo = [Member(id: 0, email: "doyun@gmail.com", kakaoId: 2, nickName: "doyun")]
+            
+            self.fetchFollow(option: .following) { error, members in
+                if let error = error {
+                    observer.onError(error)
+                }
+                
+                if let members = members {
+                    members.forEach {
+                        memberInfo.append($0)
+                    }
+                }
+                
+                memberInfo.append(Member(id: -1, email: "plus", kakaoId: 0, nickName: "+"))
+                observer.onNext(memberInfo)
+            }
             return Disposables.create()
         }
         
     }
+    
+    func fetchFollow(option:FollowFilterOptions) -> Observable<[Member]> {
+        return Observable.create { observer in
+            
+            self.fetchFollow(option: option) { error, members in
+                if let error = error {
+                    observer.onError(error)
+                }
+                
+                guard let members = members else {
+                    return observer.onNext([])
+                }
+                
+                observer.onNext(members)
+            }
+            return Disposables.create()
+        }
+    }
+    
+    private func fetchFollow(option:FollowFilterOptions, completion:@escaping((Error?,[Member]?) -> Void)) {
+        
+        let followInfo = option == .following ? "following" : "follow"
+        
+        let url = "http://52.79.87.87:9090/friend/\(followInfo)/list"
+        
+        let header = AuthService.shared.getAuthorizationHeader()
+        
+        AF.request(url, encoding: JSONEncoding.default, headers: header).responseData(completionHandler: { response in
+            switch response.result {
+            case .failure(let error) :
+                completion(error, nil)
+            case .success(let data) :
+                do {
+                    let decodedJson = try JSONDecoder().decode([Member].self, from: data)
+                    completion(nil,decodedJson)
+                }
+                catch(let error){
+                    completion(error,nil)
+                }
+            }
+        })
+    }
+    
 }
