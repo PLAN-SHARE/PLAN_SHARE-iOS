a29827c473edea0da41881c1185fa2561d0e1053
doyun0887@gmail.com <doyun>
2022-04-06 09:35:53 +0900

    :sparkles: feat : 팔로우 기능 구현을 위한 기능 추가
           body:  Follow/Unfollow시 사용할 memberResponse구조체 추가

--- PlanShare_iOS/Model/Member.swift
+++ PlanShare_iOS/Model/Member.swift
@@ -7,9 +7,22 @@
 
 import Foundation
 
+struct MemberResponse : Codable {
+    let member : Member
+    let status : Bool
+    
+}
+
 struct Member : Hashable,Codable {
     let id : Int
     let email : String
     let kakaoId : Int
     let nickName : String
+    
+    init(id:Int,email:String,kakaoId:Int,nickName:String){
+        self.id = id
+        self.email = email
+        self.kakaoId = kakaoId
+        self.nickName = nickName
+    }
 }
